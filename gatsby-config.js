1771a3301af2b8fb3b8248471ba4e64cb35f6d1f
Alex Santonastaso <alex@santonastaso.com>
2025-06-18 23:57:15 +0100

    update about me, CV, add Scrum certification

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     github: `https://github.com/snts42`,
     linkedin: `https://www.linkedin.com/in/alex-santonastaso/`,
     resume: "/Alex-Santonastaso-CV.pdf",
-    about: `Software developer with a background in Big Data Science and Computer Science. Experienced in Python for automation, data analysis, and machine learning—including automation bots, blockchain data pipelines, and facial emotion recognition. I enjoy finding efficient solutions, learning new tech, and working collaboratively to contribute to great products and services.`,
+    about: `Software Engineer with a background in Computer Science and Big Data Science. Experienced in Python for automation, data analysis, and machine learning—including automation tools, blockchain data pipelines, and facial emotion recognition. I enjoy finding efficient solutions, learning new tech, and working collaboratively to contribute to great products and services.`,
     email: "alex@santonastaso.com",
     phone: "+44 7570 280428",
     projects: [
@@ -61,6 +61,11 @@ module.exports = {
         name: 'GitHub Foundations Certification',
         description: 'Jun 2025',
         link: 'https://www.credly.com/badges/a118c3e7-f2b4-4da2-91d4-60750ff091db/linked_in?t=sy0qai', 
+      },
+      {
+        name: 'Scrum Fundamentals Certified',
+        description: 'Jun 2025',
+        link: 'https://www.scrumstudy.com/certification/verify?type=SFC&number=1085847', 
       },
       {
         name: 'NOCN Level 3 Award in Information, Advice & Guidance',
