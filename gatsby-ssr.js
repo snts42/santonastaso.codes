32f0a03c6ed3d99f4f542cdb3e33afc0305fc8b0
Alex Santonastaso <99670332+nastaso@users.noreply.github.com>
2026-06-22 01:05:40 +0100

    Disable Umami tracking script
    Comment out the Umami tracking script in onRenderBody.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key="umami"
-      defer
-      src="https://cloud.umami.is/script.js"
-      data-website-id="2369ecba-3a91-4bd8-8d51-ee6d733ffd89"
-    />,
-  ])
+  // setHeadComponents([
+  //   <script
+  //     key="umami"
+  //     defer
+  //     src="https://cloud.umami.is/script.js"
+  //     data-website-id="2369ecba-3a91-4bd8-8a51-ee6d733ffd89"
+  //   />,
+  // ])
 }
